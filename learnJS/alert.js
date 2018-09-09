9708e756c9fc210f8a8b41718886cf6e32dd1d72
Gricenko Alexandr <alexgriko29@gmail.com>
2018-09-09 16:11:05 +0300

    New files

---
+++ learnJS/alert.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/*alert('Я - Javascript!!!');
+
+...task with rename...
+
+var name, admin;
+
+name='Василий';
+admin=name;
+alert(admin);
+
+var ourPlanetName='Земля', userName='Петя'
+
+alert(ourPlanetName);
+alert(userName);
+
+var str = "Мама мыла раму";
+alert(str);
+
+var years = prompt('Сколько вам лет?',100);
+
+alert('Вам ' + years + ' лет!'*/
+
+var yourName = prompt("Как твоё наименование, сволочь?",'Никак');
+
+alert( yourName );
+
+var year = prompt('В каком году появилась спецификация ECMA-262 5.1?', '');
+
+if (year != 2011) alert( 'А вот и неправильно!' );
